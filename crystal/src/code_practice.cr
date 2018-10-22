1bb1c310abaf4ed654e5c7955322b9d19f93b786
Jakob Beckmann <beckmann_jakob@hotmail.fr>
2018-10-22 09:54:54 +0200

    added crystal for doubly linked lists

---
+++ crystal/src/code_practice.cr
@@ -0,0 +1,6 @@
+require "./code_practice/*"
+
+# TODO: Write documentation for `CodePractice`
+module CodePractice
+  # TODO: Put your code here
+end
---
+++ crystal/src/code_practice/version.cr
@@ -0,0 +1,3 @@
+module CodePractice
+  VERSION = "0.1.0"
+end
