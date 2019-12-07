a7ceecdb374439db8245dd290da7fe253920088b
Jakob Beckmann <beckmann_jakob@hotmail.fr>
2019-12-07 21:52:32 +0100

    added elixir 1.8

---
+++ elixir/lib/chapter1_8.ex
@@ -0,0 +1,32 @@
+defmodule Crack.Chapter1_8 do
+  @moduledoc """
+  1.8 String rotation:
+
+  Assume you have a method isSubstring which checks if one word is a substring
+  of another Given two strings, s1 and s2, write code to check if s2 is a
+  rotation of s1 using only one call to isSubstring (i.e. “waterbottle” is a
+  rotation of “erbottlewat”).
+  """
+
+  @doc """
+  Check if a string is a rotation of another.
+
+       iex> Crack.Chapter1_8.start("waterbottle", "erbottlewat")
+       true
+
+       iex> Crack.Chapter1_8.start("waterbottl", "erbottlewat")
+       false
+
+       iex> Crack.Chapter1_8.start("abba", "baab")
+       true
+
+       iex> Crack.Chapter1_8.start("dog", "cat")
+       false
+
+  """
+  def start(str1, str2) when is_binary(str1) and is_binary(str2) do
+    is_substring? = &String.contains?/2
+    concat = str1 <> str1
+    is_substring?.(concat, str2)
+  end
+end
---
+++ elixir/test/chapter1_8_test.exs
@@ -0,0 +1,4 @@
+defmodule Chapter1_8Test do
+  use ExUnit.Case
+  doctest Crack.Chapter1_8
+end
