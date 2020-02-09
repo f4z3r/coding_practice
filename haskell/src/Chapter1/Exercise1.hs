8dacf72a24d5edfa7014e7475f068795ca07e4ab
Jakob Beckmann <beckmann_jakob@hotmail.fr>
2020-02-09 23:30:07 +0100

    improved exercise 1 haskell

--- haskell/src/Chapter1/Exercise1.hs
+++ haskell/src/Chapter1/Exercise1.hs
@@ -6,21 +6,20 @@ import           Data.Set  (Set, empty, insert, member, notMember)
 -- Implement an algorithm to determine if a string has all unique characters. What if you
 -- cannot use additional data structures?
 containsUnique :: String -> Bool
-containsUnique "" = True
-containsUnique str = helper str empty
+containsUnique = helper empty
   where
-    helper :: String -> Set Char -> Bool
-    helper [] _ = True
-    helper (h : rest) set
+    helper :: Set Char -> String -> Bool
+    helper _ [] = True
+    helper set (h : rest)
         | null rest = h `notMember` set
         | h `member` set = False
-        | otherwise = helper rest $ insert h set
+        | otherwise = helper (insert h set) rest
 
 containsUniqueSort :: String -> Bool
 containsUniqueSort = helper . sort
   where
     helper :: String -> Bool
-    helper "" = True
+    helper [] = True
     helper [ _ ] = True
     helper (a : rest)
         | a == head rest = False
