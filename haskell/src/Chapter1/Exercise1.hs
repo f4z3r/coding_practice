24502522eef92e40735cc2a91ae411ee94b41dbe
Jakob Beckmann <beckmann_jakob@hotmail.fr>
2020-02-09 18:58:28 +0100

    added alternative solution with sorting for ch1 ex1 haskell

--- haskell/src/Chapter1/Exercise1.hs
+++ haskell/src/Chapter1/Exercise1.hs
@@ -1,6 +1,7 @@
-module Chapter1.Exercise1 ( containsUnique ) where
+module Chapter1.Exercise1 ( containsUnique, containsUniqueSort ) where
 
-import           Data.Set (Set, empty, insert, member, notMember)
+import           Data.List (sort)
+import           Data.Set  (Set, empty, insert, member, notMember)
 
 -- Implement an algorithm to determine if a string has all unique characters. What if you
 -- cannot use additional data structures?
@@ -14,3 +15,13 @@ containsUnique str = helper str empty
         | null rest = h `notMember` set
         | h `member` set = False
         | otherwise = helper rest $ insert h set
+
+containsUniqueSort :: String -> Bool
+containsUniqueSort = helper . sort
+  where
+    helper :: String -> Bool
+    helper "" = True
+    helper [ _ ] = True
+    helper (a : rest)
+        | a == head rest = False
+        | otherwise = helper rest
